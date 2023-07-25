3ff077ae41abc2afc2c9fcac83b834f6b6eddd46
Marjan Stojanovski <macikb8@yahoo.com>
2023-07-25 19:30:07 +0200

    add the analytics section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Analytics from "./components/Analytics";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Analytics />
     </>
   );
 }
